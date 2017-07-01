d72bc4ff95b989e277af634f07f8b531be76913b
Supakorn Rassameemasmuang <mistersupakorn@gmail.com>
2017-07-01 03:49:14 -0600

    fixed output and create_data changes.

--- Selective_Camera/Selective_Camera.py
+++ Selective_Camera/Selective_Camera.py
@@ -2,16 +2,16 @@ import NeuralNetwork as nn
 import PIL.Image
 
 def main():
-    img_res_side = 70
+    img_res_side = 25
     test_nn = nn.NeuralNetwork([img_res_side ** 2, 25, 1])
-    test_nn.load_data(create_data(), [1])
+    test_nn.load_data(create_data(img_res_side), [1])
     # cost = test_nn.get_cost_train(nn.NeuralNetwork.roll_vec(test_nn.params))
     #test_nn.train()
+    test_nn.train()
     test_nn.save_param('/output1.json')
     pass
 
 def create_data(img_res_side):
-    img_res_side = 25
     test_img = PIL.Image.open('data/raw_data/atty.jpg')
     test_img = test_img.resize((img_res_side,img_res_side)).convert('L')
     test_list = []
